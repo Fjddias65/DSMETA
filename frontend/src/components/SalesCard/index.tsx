c5f905d7131be938eced4def60f8bc42e2a88ad4
Fjddias65 <fjddias@outlook.com>
2022-07-14 22:29:21 -0300

    useSate.

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,5 +1,5 @@
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 import './styles.css';
 import NotificationButton from '../NotificationButton';
 import { useState } from "react";
