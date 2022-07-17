0b0073dbf172105e029a5ca79acd5a2a116711e7
Fjddias65 <fjddias@outlook.com>
2022-07-17 07:49:29 -0300

    Send notification.

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -78,12 +78,12 @@ function SalesCard() {
                                     <td>{sale.amount.toFixed(2)}</td>
                                     <td>
                                         <div className="dsmeta-red-btn-container">
-                                            <NotificationButton />
+                                            <NotificationButton saleId={sale.id} />
                                         </div>
                                     </td>
                                 </tr>
-                            )
-                        })
+                                )
+                            })
                         }
 
                     </tbody>
